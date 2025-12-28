9297e182b7be425e74510edb2402590fb31ed67f
Kaden Barlow <kadenbarlow@gmail.com>
2025-12-27 21:02:26 -0700

    add chrome extension icons

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,10 @@
 import path from "path"
 import { defineConfig } from "vite"
-import { viteStaticCopy } from "vite-plugin-static-copy"
 import react from "@vitejs/plugin-react-swc"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [
-        { dest: ".", src: "manifest.json" },
-        { dest: ".", src: "devtools.html" },
-        { dest: ".", src: "src/main.js" },
-      ],
-    }),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       "#library": path.resolve(__dirname, "./src/library"),
